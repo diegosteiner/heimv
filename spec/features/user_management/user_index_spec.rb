4d7f8658adb167c06eebc17d76b8680f5739afe6
Diego Steiner <diego.steiner@renuo.ch>
2017-12-04 18:55:47 +0100

    Cleanup

--- spec/features/user_management/user_index_spec.rb
+++ spec/features/user_management/user_index_spec.rb
@@ -3,7 +3,7 @@
 include Warden::Test::Helpers
 Warden.test_mode!
 
-feature 'User index page', :devise do
+feature 'User Management page', :devise do
   after(:each) do
     Warden.test_reset!
   end
