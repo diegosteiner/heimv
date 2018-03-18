bbb25c266027fcf3d5358e41ea438af3d5323f5e
Diego Steiner <diego.steiner@u041.ch>
2018-03-18 21:12:48 +0100

    Improve dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   end
 
   namespace :manage do
-    get 'dashboard', to: 'dashboard#index'
+    get '/', to: 'dashboard#index', as: :manage_dashboard
     resources :homes
     resources :bookings do
       resources :contracts, shallow: true
