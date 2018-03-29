0ba7737df95858526f833f2983f558c4e25016b4
Diego Steiner <diego.steiner@u041.ch>
2018-03-29 15:16:35 +0200

    Fix mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,6 +58,7 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "heimverwaltung_#{Rails.env}"
+  config.action_mailer.delivery_method = nil
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
