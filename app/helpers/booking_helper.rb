c35b4361d9158f7d9cb2b9e7be1ff2f8231c3568
Diego Steiner <diego.steiner@u041.ch>
2018-10-15 14:34:27 +0200

    Fix rubocop offenses

--- app/helpers/booking_helper.rb
+++ app/helpers/booking_helper.rb
@@ -7,11 +7,11 @@ module BookingHelper
     view_model.allowed_transitions_for_select
   end
 
-  def bookings_by_state_table(state, bookings, special_column= nil, help_text= nil, &block)
+  def bookings_by_state_table(state, bookings, special_column = nil, help_text = nil, &block)
     safe_join([
-      tag.h3(BookingStrategy::Default::ViewModel.i18n_state(state)[:label]),
-      tag.span(help_text),
-      render(layout: 'table', locals: { bookings: bookings, special_column: special_column }, &block)
-    ])
+                tag.h3(BookingStrategy::Default::ViewModel.i18n_state(state)[:label]),
+                tag.span(help_text),
+                render(layout: 'table', locals: { bookings: bookings, special_column: special_column }, &block)
+              ])
   end
 end
