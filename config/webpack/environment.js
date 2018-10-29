d178d930f2cf1bd306ed2f74594312fe2641cbbe
Diego Steiner <diego.steiner@u041.ch>
2018-10-29 13:51:26 +0100

    Upgrade yarn and vue config

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -6,8 +6,8 @@ const vue = require('./loaders/vue')
 const webpack = require('webpack')
 
 environment.plugins.prepend('Provide', new webpack.ProvidePlugin({
-  $: 'jquery',
-  jQuery: 'jquery',
+  // $: 'jquery',
+  // jQuery: 'jquery',
   // 'window.Tether': "tether",
   // Popper: ['popper.js', 'default'], // for Bootstrap 4
 }))
@@ -21,7 +21,17 @@ environment.loaders.insert('font-awesome', {
   loader: "file-loader"
 })
 
-// environment.plugins.append('VueLoaderPlugin', new VueLoaderPlugin())
+const envConfig = environment.loaders.append('vue', vue)
+module.exports = environment
+const aliasConfig = environment.loaders.append('vue', vue)
+module.exports = {
+  resolve: {
+    alias: {
+      jquery: 'jquery/src/jquery'
+    }
+  }
+}
+
 environment.loaders.append('vue', vue)
 module.exports = environment
 
--- lib/tasks/assets.rake
+++
@@ -1,9 +0,0 @@
-namespace :assets do
-  # task precompile: :environment do
-  #   Rake::Task['webpacker:compile'].invoke
-  # end
-
-  task clean: :environment do
-  end
-
-end
