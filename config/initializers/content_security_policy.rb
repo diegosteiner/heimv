0335ffad8051edcc5b023be179b205e204562669
Diego Steiner <diego.steiner@u041.ch>
2018-11-24 09:29:45 +0100

    Fix Vue production error

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -17,7 +17,7 @@ Rails.application.config.content_security_policy do |policy|
   if Rails.env.development?
     policy.script_src :self, :https, :unsafe_eval
   else
-    policy.script_src :self, :https
+    policy.script_src :self, :https, :unsafe_eval
   end
 end
 
