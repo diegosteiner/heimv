79420550de7706395c90a2421c8211634425c42c
Diego Steiner <diego.steiner@u041.ch>
2019-05-03 15:08:11 +0200

    Fix rubocop issues

--- app/models/occupancy/filter.rb
+++ app/models/occupancy/filter.rb
@@ -5,10 +5,10 @@ class Occupancy
     attribute :ends_at_after, :datetime
     attribute :ends_at_before, :datetime
 
-    filter do |occupancy, f|
+    filter do |_occupancy, f|
       Occupancy.where.not(booking: nil)
-                .begins_at(after: f.begins_at_after, before: f.begins_at_before)
-                .ends_at(after: f.ends_at_after, before: f.ends_at_before)
+               .begins_at(after: f.begins_at_after, before: f.begins_at_before)
+               .ends_at(after: f.ends_at_after, before: f.ends_at_before)
     end
   end
 end
