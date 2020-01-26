02106f4ebd57aca645097df7a9562128bdfab01f
Diego Steiner <diego.steiner@u041.ch>
2020-01-26 19:25:58 +0100

    Fix migrations

--- db/migrate/20200117142649_add_tarif_selector_type_to_tarif_tarif_selector.rb
+++ db/migrate/20200117142649_add_tarif_selector_type_to_tarif_tarif_selector.rb
@@ -2,9 +2,9 @@ class AddTarifSelectorTypeToTarifTarifSelector < ActiveRecord::Migration[6.0]
   def up
     add_column :tarif_tarif_selectors, :tarif_selector_type, :string, index: true, null: true
 
-    TarifTarifSelector.find_each do |tarif_tarif_selector|
-      tarif_tarif_selector.update(tarif_selector_type: tarif_tarif_selector.tarif_selector.class.to_s)
-    end
+    # TarifTarifSelector.find_each do |tarif_tarif_selector|
+    #   tarif_tarif_selector.update(tarif_selector_type: tarif_tarif_selector.tarif_selector.class.to_s)
+    # end
   end
 
   def down
