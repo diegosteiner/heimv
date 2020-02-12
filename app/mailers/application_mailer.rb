7ad408dcdbacd7f8940ac4cdb5189893d8c00d23
Diego Steiner <diego.steiner@u041.ch>
2020-02-12 14:20:34 +0100

    Fix semaphore

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,19 +1,5 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  def self.delivery_method_settings=(delivery_method_settings)
-    raise 'MAILER_URL is invalid' unless delivery_method_settings.valid?
-
-    @delivery_method_settings = delivery_method_settings
-
-    ActionMailer::Base.delivery_method = delivery_method_settings.delivery_method
-    ActionMailer::Base.try(delivery_method_settings.settings_method_name, delivery_method_settings.to_h)
-
-    default from: -> { delivery_method_settings.fetch(:from, 'test@heimv.ch') },
-            bcc: -> { delivery_method_settings[:bcc] }
-  end
-
-  self.delivery_method_settings = DeliveryMethodSettings.new(ENV['MAILER_URL']) if delivery_method.nil?
   layout 'mailer'
-  default from: -> { 'test@heimv.ch' }
 end
