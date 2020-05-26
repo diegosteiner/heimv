ad32911c02bb8fc4efdcc7bbb04506895aad903e
Diego Steiner <diego.steiner@u041.ch>
2020-05-26 17:23:54 +0200

    Fix PDF for data digests

--- app/params/manage/usage_params.rb
+++ app/params/manage/usage_params.rb
@@ -1,7 +1,7 @@
 module Manage
   class UsageParams < ApplicationParams
     def self.permitted_keys
-      %i[tarif_id booking_id used_units remarks] +
+      %i[tarif_id booking_id presumed_used_units used_units remarks] +
         [{ meter_reading_period_attributes: MeterReadingPeriodParams.permitted_keys + %i[id _destroy] }]
     end
   end
