9711f729e77233503cba259023c413ac6b861f43
Diego Steiner <diego.steiner@u041.ch>
2020-06-21 16:28:47 +0200

    Fix pdfs

--- app/services/export/pdf/data_digest_period_pdf.rb
+++ app/services/export/pdf/data_digest_period_pdf.rb
@@ -21,10 +21,18 @@ module Export
       end
 
       to_render do
-        table_data = [@data_digest_period.data_header] + @data_digest_period.data
-        column_widths = @options[:column_widths] || {}
+        from = data_digest_period.period_range.begin
+        to = data_digest_period.period_range.end
+        text([
+          (from && I18n.t('data_digests.period_from', date: I18n.l(from))),
+          (to && I18n.t('data_digests.period_to', date: I18n.l(to)))
+        ].compact.join(' '), size: 8)
+        move_down 10
+      end
 
-        table(table_data, column_widths: column_widths) do
+      to_render do
+        table_data = [@data_digest_period.data_header] + @data_digest_period.data
+        table(table_data, width: bounds.width) do
           cells.style(size: 8, borders: [])
           row(0).font_style = :bold
           row(0).borders = [:bottom]
