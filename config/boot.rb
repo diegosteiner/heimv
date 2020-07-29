36a2ec68525c54aa0040f5d8c8db399348459747
Diego Steiner <diego.steiner@u041.ch>
2020-07-29 10:36:04 +0200

    Disable bootsnap

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,7 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
+require 'objspace'
+ObjectSpace.trace_object_allocations_start
+
 require 'bundler/setup' # Set up gems listed in the Gemfile.
-require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
+#require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
--- config/initializers/rack_profiler.rb
+++ config/initializers/rack_profiler.rb
@@ -1,6 +1,3 @@
 # frozen_string_literal: true
 
 Rack::MiniProfiler.config.enable_advanced_debugging_tools = true
-
-require 'objspace'
-ObjectSpace.trace_object_allocations_start
