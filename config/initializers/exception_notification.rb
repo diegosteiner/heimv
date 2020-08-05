220391547977de612a917d86b65ad5c812e05d17
Diego Steiner <diego.steiner@u041.ch>
2020-08-05 23:26:30 +0200

    Bump Version

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -3,7 +3,6 @@
 if Rails.env.production?
   Rails.application.config.middleware.use ExceptionNotification::Rack,
                                           email: {
-
                                             sender_address: ENV['MAIL_FROM'],
                                             exception_recipients: Array.wrap(ENV['EXCEPTION_RECIPIENTS']),
                                             delivery_method: :smtp,
