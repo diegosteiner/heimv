2a382b6c40be9c14e48dfced2b0471050f4806a4
Diego Steiner <diego.steiner@u041.ch>
2020-11-23 10:23:57 +0000

    Add locales fr, it

--- app/params/manage/booking_params.rb
+++ app/params/manage/booking_params.rb
@@ -4,7 +4,8 @@ module Manage
   class BookingParams < Public::BookingParams::Create
     def self.permitted_keys
       super +
-        %i[tenant_id transition_to notifications_enabled internal_remarks cancellation_reason timeframe_locked] +
+        %i[tenant_id transition_to notifications_enabled internal_remarks cancellation_reason editable
+           timeframe_locked] +
         [occupancy_attributes: Public::OccupancyParams.permitted_keys] +
         [usages_attributes: UsageParams.permitted_keys + %i[_destroy id]] +
         [tenant_attributes: TenantParams.permitted_keys] +
