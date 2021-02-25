a43a52f60ec595a63a77baacf960063702125b6f
Diego Steiner <diego.steiner@u041.ch>
2021-02-25 20:15:42 +0000

    Update seeds

---
+++ app/services/import/booking_purpose_importer.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+module Import
+  class BookingPurposeImporter < Base
+
+    def relevant_attributes
+      %w[key title_i18n]
+    end
+
+    protected
+
+    def to_h
+      organisation.booking_purposes.find_each.map do |purpose|
+        [purpose.key, purpose.attributes.slice(*relevant_attributes)]
+      end.to_h
+    end
+
+    def from_h(serialized)
+      serialized.map do |key, attributes|
+        purpose = organisation.booking_purposes.find_or_initialize_by(key: ley)
+        purpose.update(attributes.slice(*relevant_attributes)) if purpose.new_record? || options[:replace]
+        purpose if purpose.valid?
+      end
+    end
+  end
+end
