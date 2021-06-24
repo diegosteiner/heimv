a3ffe714e7372c51903dd15eb44fa11cd34cce62
Diego Steiner <diego.steiner@u041.ch>
2021-06-24 12:23:40 +0000

    Allow commit_request in overdue_request

--- app/domain/booking_actions/public/commit_request.rb
+++ app/domain/booking_actions/public/commit_request.rb
@@ -10,7 +10,7 @@ module BookingActions
       def allowed?
         booking.valid?(context: :public_update) &&
           booking.tenant&.complete? &&
-          booking.booking_flow.in_state?(:provisional_request) &&
+          booking.booking_flow.in_state?(:provisional_request, :overdue_request) &&
           !booking.committed_request
       end
 
