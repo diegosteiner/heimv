1a38f29f2e29d03ab831e731adfd1f04faa29f90
Diego Steiner <diego.steiner@u041.ch>
2022-02-02 16:45:02 +0000

    Improve data digest

--- app/models/data_digests/parahotelerie_statistics.rb
+++ app/models/data_digests/parahotelerie_statistics.rb
@@ -30,6 +30,7 @@ module DataDigests
       [
         ::Occupancy.human_attribute_name(:begins_at), ::Occupancy.human_attribute_name(:ends_at),
         I18n.t('data_digests/parahotelerie_statistics.total_overnight_stays'),
+        I18n.t('data_digests/parahotelerie_statistics.nights'),
         ::Tenant.human_attribute_name(:country_code)
       ]
     end
@@ -40,7 +41,7 @@ module DataDigests
 
     def build_data_row(booking)
       [I18n.l(booking.occupancy.begins_at, format: :short), I18n.l(booking.occupancy.ends_at, format: :short),
-       headcount(booking), booking.tenant&.country_code]
+       headcount(booking), booking.occupancy.nights, booking.tenant&.country_code]
     end
   end
 end
