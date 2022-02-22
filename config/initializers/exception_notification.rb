0d299bc026b0030a07b6c0d54435bf3feeafbbc1
Diego Steiner <diego.steiner@u041.ch>
2022-02-22 09:09:16 +0000

    fix: correctly add exception_notification

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -1,12 +1,14 @@
 # frozen_string_literal: true
 
-Rails.application.config.to_prepare do
-  if defined?(ExceptionNotification)
-    Rails.application.config.middleware.use ExceptionNotification::Rack,
-                                            email: {
-                                              email_prefix: "[#{ENV['APP_HOST']}] ",
-                                              exception_recipients: ENV.fetch('EXCEPTION_RECIPIENTS',
-                                                                              'info@heimv.ch').split(',')
-                                            }
-  end
+if defined?(ExceptionNotification)
+  Rails.application.config.middleware.use ExceptionNotification::Rack,
+                                          email: {
+                                            email_prefix: "[#{ENV['APP_HOST']}] ",
+                                            exception_recipients: ENV.fetch('EXCEPTION_RECIPIENTS',
+                                                                            'info@heimv.ch').split(',')
+                                          }
+end
+
+defined?(Sentry) && ENV['SENTRY_DSN'].present? && Sentry.init do |config|
+  config.dsn = ENV['SENTRY_DSN']
 end
