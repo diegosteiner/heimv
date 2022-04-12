acda1eb1ed82c4f35bb261801778e3e201818c3f
Diego Steiner <diego.steiner@u041.ch>
2022-04-12 14:55:09 +0000

    fix: deprecation warning from capybara

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -2,27 +2,17 @@
 
 Capybara.register_driver :selenium do |app|
   options = Selenium::WebDriver::Chrome::Options.new
+  options.add_preference(:browser, set_download_behavior: { behavior: 'allow' })
   options.add_preference(:download, prompt_for_download: false,
                                     default_directory: '/tmp/downloads')
 
-  options.add_preference(:browser, set_download_behavior: { behavior: 'allow' })
   options.add_argument('--headless')
   options.add_argument('--window-size=1280,1024')
   options.add_argument('--no-default-browser-check')
   options.add_argument('--start-maximized')
 
   url = "http://#{ENV['SELENIUM_HOST']}/wd/hub"
-  Capybara::Selenium::Driver.new(app, browser: :chrome, url: url, options: options)
-end
-
-Capybara.register_driver :headless_chrome do |app|
-  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-    chromeOptions: {
-      args: %w[headless enable-features=NetworkService,NetworkServiceInProcess]
-    }
-  )
-
-  Capybara::Selenium::Driver.new app, browser: :chrome, desired_capabilities: capabilities
+  Capybara::Selenium::Driver.new(app, browser: :chrome, url: url, capabilities: [options])
 end
 
 Capybara.run_server = ENV['E2E_SERVER_PORT'].present?
