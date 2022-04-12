e6dced63ba7545b4aa8494b8437f68012cfdce90
Diego Steiner <diego.steiner@u041.ch>
2022-04-12 14:54:10 +0000

    feature: add settings for homes and organisations

--- app/serializers/manage/home_serializer.rb
+++ app/serializers/manage/home_serializer.rb
@@ -2,10 +2,13 @@
 
 module Manage
   class HomeSerializer < Public::HomeSerializer
-    fields :janitor, :address, :min_occupation
+    fields :janitor, :address
 
     view :export do
       fields(*Import::Hash::HomeImporter.used_attributes.map(&:to_sym))
+      field :settings do |home|
+        home.settings.to_h
+      end
       association :tarifs, blueprint: Manage::TarifSerializer, view: :export
     end
   end
