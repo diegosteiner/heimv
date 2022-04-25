2c67c3b77ad97cc309a985f48afd8afe030c1bf1
Diego Steiner <diego.steiner@u041.ch>
2022-04-25 10:24:22 +0000

    feature: add more help content

--- app/controllers/manage/pages_controller.rb
+++ app/controllers/manage/pages_controller.rb
@@ -16,15 +16,27 @@ module Manage
         BookingStates::CancelationPending, BookingStates::Cancelled
       ]
 
-      @rich_text_templates = rich_text_templates_by_requirer
+      @rich_text_templates = rich_text_templates_for_help
     end
 
+    private
+
     def rich_text_templates_by_requirer
-      RichTextTemplate.required_templates.values.flatten.each_with_object({}) do |required, memo|
+      rich_text_templates = Hash.new { |hash, key| hash[key] = [] }
+      RichTextTemplate.required_templates.values.flatten.each_with_object(rich_text_templates) do |required, memo|
         next unless required.required_by
 
-        memo[required.required_by] ||= []
         memo[required.required_by] << required.key
+      end
+    end
+
+    def rich_text_templates_for_help
+      rich_text_templates_by_requirer.tap do |rich_text_templates|
+        rich_text_templates[BookingStates::DefinitiveRequest] << :contract_text
+        rich_text_templates[BookingStates::DefinitiveRequest] << :invoices_deposit_text
+        rich_text_templates[BookingStates::AwaitingContract] << :awaiting_contract_notification
+        rich_text_templates[BookingStates::Past] << :invoices_invoice_text
+        rich_text_templates[BookingStates::Past] << :payment_due_notification
       end
     end
   end
