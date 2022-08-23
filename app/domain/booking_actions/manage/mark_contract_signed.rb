82e0b2f2f6f7fc8f7b59eb7288c5454bc3990d8c
Diego Steiner <diego.steiner@u041.ch>
2022-08-23 19:31:01 +0000

    fix: specs

--- app/domain/booking_actions/manage/mark_contract_signed.rb
+++ app/domain/booking_actions/manage/mark_contract_signed.rb
@@ -6,11 +6,12 @@ module BookingActions
       RichTextTemplate.require_template(:contract_signed_notification, context: %i[booking], required_by: self)
 
       def call!
-        if Invoices::Deposit.of(booking).kept.unpaid.exists?
-          booking.notifications.new(template: :contract_signed_notification, to: booking.tenant).deliver
-        end
-
         booking.contract.signed!
+        booking.update(committed_request: true)
+
+        return unless Invoices::Deposit.of(booking).kept.unpaid.exists?
+
+        booking.notifications.new(template: :contract_signed_notification, to: booking.tenant).deliver
       end
 
       def allowed?
