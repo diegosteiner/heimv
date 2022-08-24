4ff5feb700e4ff9abcc64cbef03caa4b6dfc7554
Diego Steiner <diego.steiner@u041.ch>
2022-08-24 19:39:38 +0000

    fix: caching in production

--- config/environment.rb
+++ config/environment.rb
@@ -5,3 +5,17 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+Rails.application.configure do
+  redis_url = ENV.slice('REDIS_TLS_URL', 'REDIS_URL').values.compact.first
+
+  if redis_url.present?
+    config.cache_store = :redis_cache_store, { url: redis_url,
+                                               namespace: :heimv_cache,
+                                               connect_timeout: 30, # Defaults to 20 seconds
+                                               read_timeout: 0.2, # Defaults to 1 second
+                                               write_timeout: 0.2, # Defaults to 1 second
+                                               reconnect_attempts: 1 } # ,   # Defaults to 0
+
+  end
+end
