e9590206f98d802b2e7a47edaa6b1e71c9551d38
Diego Steiner <diego.steiner@u041.ch>
2022-09-07 12:51:33 +0000

    fix: broken migration

--- db/migrate/20220823091706_replace_booking_copy_tarifs.rb
+++ db/migrate/20220823091706_replace_booking_copy_tarifs.rb
@@ -2,8 +2,12 @@ class ReplaceBookingCopyTarifs < ActiveRecord::Migration[7.0]
   def change
     add_column :usages, :price_per_unit, :decimal, null: true
 
-    pin_price_per_unit
-    flatten_usage_tarifs
+    reversible do |direction|
+      direction.up do 
+        pin_price_per_unit
+        flatten_usage_tarifs
+      end
+    end
 
     # remove_reference :tarifs, :booking_copy_template, index: true, foreign_key: true
     # remove_foreign_key :tarifs, column: :booking_copy_template_id, to_table: :tarifs
@@ -13,24 +17,24 @@ class ReplaceBookingCopyTarifs < ActiveRecord::Migration[7.0]
   private 
 
   def pin_price_per_unit
-    reversible do |direction|
-      direction.up do 
-        Usage.find_each do |usage|
-          usage.pin_price_per_unit && usage.save
-        end
-      end
+    Usage.find_each do |usage|
+      usage.pin_price_per_unit && usage.save
     end
   end
 
   def flatten_usage_tarifs
-    reversible do |direction|
-      direction.up do 
-        Tarif.where.not(booking_id: nil).find_each do |tarif|
-          usage = Usage.find_by(booking_id: tarif.booking_id, tarif_id: tarif.id)
-          usage&.update_columns(tarif_id: tarif.booking_copy_template_id)
-          tarif.destroy
-        end
+    Tarif.where.not(booking_id: nil).find_each do |tarif|
+      usage = Usage.find_by(booking_id: tarif.booking_id, tarif_id: tarif.id)
+      target_tarif_id = tarif.booking_copy_template_id
+      other_usage = usage.booking.usages.where(tarif_id: target_tarif_id).take if usage.present?
+
+      if other_usage.present?
+        other_usage.destroy unless other_usage.used_units&.nonzero?
+        usage.destroy unless other_usage.destroyed?
       end
+
+      usage&.update_columns(tarif_id: target_tarif_id) unless usage&.destroyed?
+      tarif.destroy
     end
   end
 end
