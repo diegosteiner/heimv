543f74b6359fdf4050ce22d15403567bf82d3d51
Diego Steiner <diego.steiner@u041.ch>
2023-01-14 20:36:52 +0000

    feature: add sidekiq-cron

--- app/jobs/clean_data_digests_job.rb
+++ app/jobs/clean_data_digests_job.rb
@@ -4,6 +4,6 @@ class CleanDataDigestsJob < ApplicationJob
   queue_as :default
 
   def perform
-    DataDigest.where(DataDigest.arel_table[:created_at].lt(3.days.ago)).destroy_all
+    DataDigest.where(DataDigest.arel_table[:created_at].lt(14.days.ago)).destroy_all
   end
 end
