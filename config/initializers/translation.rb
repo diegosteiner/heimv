2bfa63ad6b22e274fd49ab8386806d8838f07f8d
Diego Steiner <diego.steiner@u041.ch>
2023-03-17 11:21:54 +0100

    fix: remove untranslated it strings

--- config/initializers/translation.rb
+++ config/initializers/translation.rb
@@ -4,7 +4,7 @@ if ENV['TRANSLATION_IO_API_KEY'].present?
   TranslationIO.configure do |config|
     config.api_key = ENV.fetch('TRANSLATION_IO_API_KEY')
     config.source_locale = :de
-    config.target_locales = %i[fr it]
+    config.target_locales = %i[fr]
     config.disable_gettext = true
     config.charset = 'UTF-8'
   end
