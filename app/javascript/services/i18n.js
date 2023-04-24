cf0b07d8b26d3ac9436102eaa5386c24c787c5d2
Diego Steiner <diego.steiner@u041.ch>
2023-04-24 11:07:51 +0000

    fix: locale in calendar

--- app/javascript/services/i18n.js
+++ app/javascript/services/i18n.js
@@ -15,7 +15,7 @@ i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
     resources,
-    lng: "de",
+    lng: document?.documentElement?.lang?.split("-")?.[0] || "de",
     debug: true,
     keySeparator: ".",
 
