e6c2d6c627b143c1e7b517ebae095ba5932bb129
Diego Steiner <diego.steiner@u041.ch>
2023-07-25 13:16:57 +0200

    fix: retry deserialization errors

--- app/jobs/mail_delivery_job.rb
+++ app/jobs/mail_delivery_job.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 class MailDeliveryJob < ActionMailer::MailDeliveryJob
-  retry_on Net::SMTPFatalError, Net::SMTPAuthenticationError, Net::ReadTimeout, wait: 30.seconds
+  retry_on Net::SMTPFatalError, Net::SMTPAuthenticationError, Net::ReadTimeout, ActiveJob::DeserializationError,
+           wait: 30.seconds
   # discard_on ActiveJob::DeserializationError
   sidekiq_options retry: 5
 end
