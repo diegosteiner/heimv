84ecdf6b3e3dac5c26d9f969db7c8c76a5798453
Diego Steiner <diego.steiner@u041.ch>
2023-11-14 08:23:57 +0000

    fix: booking flow help

--- app/controllers/manage/pages_controller.rb
+++ app/controllers/manage/pages_controller.rb
@@ -7,7 +7,20 @@ module Manage
     def usage; end
 
     def flow
-      @states = [
+      @templates = booking_states.index_with do |booking_state|
+        booking_state.templates.pluck(:key)
+      end
+      @templates[BookingStates::DefinitiveRequest] << :contract_text
+      @templates[BookingStates::DefinitiveRequest] << :invoices_deposit_text
+      @templates[BookingStates::AwaitingContract] << :awaiting_contract_notification
+      @templates[BookingStates::Past] << :invoices_invoice_text
+      @templates[BookingStates::Past] << :payment_due_notification
+    end
+
+    private
+
+    def booking_states
+      @booking_states = [
         BookingStates::UnconfirmedRequest, BookingStates::OpenRequest, BookingStates::ProvisionalRequest,
         BookingStates::OverdueRequest, BookingStates::DefinitiveRequest, BookingStates::CancelledRequest,
         BookingStates::DeclinedRequest, BookingStates::AwaitingContract,
@@ -15,29 +28,6 @@ module Manage
         BookingStates::Past, BookingStates::PaymentDue, BookingStates::PaymentOverdue, BookingStates::Completed,
         BookingStates::CancelationPending, BookingStates::Cancelled
       ]
-
-      @rich_text_templates = rich_text_templates_for_help
-    end
-
-    private
-
-    def rich_text_templates_by_requirer
-      rich_text_templates = Hash.new { |hash, key| hash[key] = [] }
-      RichTextTemplate.definitions.values.flatten.each_with_object(rich_text_templates) do |required, memo|
-        next unless required.required_by
-
-        memo[required.required_by] << required.key
-      end
-    end
-
-    def rich_text_templates_for_help
-      rich_text_templates_by_requirer.tap do |rich_text_templates|
-        rich_text_templates[BookingStates::DefinitiveRequest] << :contract_text
-        rich_text_templates[BookingStates::DefinitiveRequest] << :invoices_deposit_text
-        rich_text_templates[BookingStates::AwaitingContract] << :awaiting_contract_notification
-        rich_text_templates[BookingStates::Past] << :invoices_invoice_text
-        rich_text_templates[BookingStates::Past] << :payment_due_notification
-      end
     end
   end
 end
