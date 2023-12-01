e362cb22a77e0a148b0cbc3b33cac7f338ce07a1
Diego Steiner <diego.steiner@u041.ch>
2023-12-01 15:51:24 +0000

    fix: migration

--- db/migrate/20231201125422_add_autodeliver_to_rich_text_templates.rb
+++ db/migrate/20231201125422_add_autodeliver_to_rich_text_templates.rb
@@ -1,12 +1,14 @@
 class AddAutodeliverToRichTextTemplates < ActiveRecord::Migration[7.1]
   def change
+    Rails.application.eager_load!
+
     add_column :rich_text_templates, :autodeliver, :boolean, default: true
 
     reversible do |direction|
       RichTextTemplate.definitions.each do |key, definition|
         next if definition[:autodeliver].nil? || definition[:autodeliver]
 
-        Notification.where(key:).update_all(autodeliver: definition[:autodeliver])
+        RichTextTemplate.where(key:).update_all(autodeliver: definition[:autodeliver])
       end
     end
   end
