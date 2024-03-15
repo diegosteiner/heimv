a2ba3c21f9f9a21bfbf12e68cab802507bd6302a
Diego Steiner <diego.steiner@u041.ch>
2024-03-15 09:35:21 +0000

    fix: tinymce and liquid tags

--- app/services/rich_text_sanitizer.rb
+++ app/services/rich_text_sanitizer.rb
@@ -1,13 +1,23 @@
 # frozen_string_literal: true
 
 class RichTextSanitizer
+  class Scrubber < Loofah::Scrubber
+    def scrub(node)
+      if node.name == 'script' && node['type'] == 'application/liquid'
+        Loofah::Scrubber::CONTINUE
+      else
+        Loofah::Scrubber::STOP
+      end
+    end
+  end
+
   def self.sanitize(html)
     sanitizer = Rails::Html::SafeListSanitizer.new
-    sanitizer.sanitize(html, { tags: allowed_tags, attributes: allowed_attributes })
+    sanitizer.sanitize(html, { tags: allowed_tags, attributes: allowed_attributes, scrubber: Scrubber.new })
   end
 
   def self.allowed_tags
-    Rails::Html::SafeListSanitizer.allowed_tags + %w[table tr td th thead tbody liquid]
+    Rails::Html::SafeListSanitizer.allowed_tags + %w[table tr td th thead tbody span]
   end
 
   def self.allowed_attributes
