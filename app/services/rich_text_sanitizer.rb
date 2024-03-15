03049ce2715b75cc2eb9da121f7b2596bc06c5fe
Diego Steiner <diego.steiner@u041.ch>
2024-03-15 09:54:53 +0000

    fix: allow liquid tags

--- app/services/rich_text_sanitizer.rb
+++ app/services/rich_text_sanitizer.rb
@@ -1,23 +1,13 @@
 # frozen_string_literal: true
 
 class RichTextSanitizer
-  class Scrubber < Loofah::Scrubber
-    def scrub(node)
-      if node.name == 'script' && node['type'] == 'application/liquid'
-        Loofah::Scrubber::CONTINUE
-      else
-        Loofah::Scrubber::STOP
-      end
-    end
-  end
-
   def self.sanitize(html)
     sanitizer = Rails::Html::SafeListSanitizer.new
-    sanitizer.sanitize(html, { tags: allowed_tags, attributes: allowed_attributes, scrubber: Scrubber.new })
+    sanitizer.sanitize(html, { tags: allowed_tags, attributes: allowed_attributes })
   end
 
   def self.allowed_tags
-    Rails::Html::SafeListSanitizer.allowed_tags + %w[table tr td th thead tbody span]
+    Rails::Html::SafeListSanitizer.allowed_tags + %w[table tr td th thead tbody span liquid]
   end
 
   def self.allowed_attributes
