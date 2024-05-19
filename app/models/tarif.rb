25484962fcc73248507156dabfa01999aec94695
Diego Steiner <diego.steiner@u041.ch>
2024-05-19 16:06:31 +0000

    fix: pld price_per_unit for tarifs

--- app/models/tarif.rb
+++ app/models/tarif.rb
@@ -154,7 +154,8 @@ class Tarif < ApplicationRecord
   def breakdown_options(usage)
     {
       used_units: number_to_rounded(usage.used_units || 0, precision: 2, strip_insignificant_zeros: true),
-      unit:, price_per_unit: number_to_currency(price_per_unit || 0, unit: organisation.currency)
+      price_per_unit: number_to_currency(usage.price_per_unit || 0, unit: organisation.currency),
+      unit:
     }
   end
 
