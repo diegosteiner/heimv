588d26768b5004fa2781a0e91903eaf3f63abf11
Diego Steiner <diego.steiner@u041.ch>
2024-08-06 08:12:34 +0000

    chore: update dependencies

--- app/jobs/mail_delivery_job.rb
+++ app/jobs/mail_delivery_job.rb
@@ -1,8 +1,12 @@
 # frozen_string_literal: true
 
 class MailDeliveryJob < ActionMailer::MailDeliveryJob
-  retry_on Net::SMTPFatalError, Net::SMTPAuthenticationError, Net::ReadTimeout, ActiveJob::DeserializationError,
-           wait: 30.seconds
+  retry_on Net::SMTPFatalError,
+           Net::SMTPAuthenticationError,
+           Net::ReadTimeout,
+           ActiveJob::DeserializationError,
+           Errno::ECONNREFUSED,
+           wait: 30.seconds, attempts: 5
   # discard_on ActiveJob::DeserializationError
   sidekiq_options retry: 5
 end
