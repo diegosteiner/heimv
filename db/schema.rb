5acf80f155617ed4061d8adb7d3add4878b99ff6
Diego Steiner <diego.steiner@u041.ch>
2024-10-09 10:08:07 +0000

    chore: bump version

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_08_064222) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_09_093906) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
