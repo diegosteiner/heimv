5fd29423658aa93dfe857172b9b5b7f52a4fe937
Diego Steiner <diego.steiner@u041.ch>
2024-11-25 09:07:01 +0000

    fix: migration

--- db/migrate/20241125082419_remove_deadline_id_for_bookings.rb
+++ db/migrate/20241125082419_remove_deadline_id_for_bookings.rb
@@ -3,7 +3,7 @@ class RemoveDeadlineIdForBookings < ActiveRecord::Migration[7.2]
     reversible do |direction|
       direction.up do
         # remove all non referenced deadlines
-        Deadline.where.not(id: Booking.plick(:deadline_id)).delete_all
+        Deadline.where.not(id: Booking.pluck(:deadline_id)).delete_all
       end
     end
 
