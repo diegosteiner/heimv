a1318e43b99e6369491eb9dc2d1324825427b467
Diego Steiner <diego.steiner@u041.ch>
2024-12-10 14:54:56 +0000

    fix: colums config form

--- app/javascript/components/data_digest_templates/ColumnsConfigForm.tsx
+++ app/javascript/components/data_digest_templates/ColumnsConfigForm.tsx
@@ -16,21 +16,22 @@ type ColumnsConfigFormProps = {
   name: string;
 };
 
-type DefaultColumnConfig = {
+type BaseColumnConfig = {
   id: string;
   index: number;
-  type: ColumnConfigType;
   header: string;
   body: string;
+  type: ColumnConfigType;
 };
 
-type UsageColumnConfig = DefaultColumnConfig & {
+type UsageColumnConfig = BaseColumnConfig & {
   type: ColumnConfigType.Usage;
-  tarif_id: number;
+  tarif_id: string | number;
 };
 
-type CostColumnConfig = DefaultColumnConfig & {
-  type: "costs";
+type BookingQuestionResponseColumnConfig = BaseColumnConfig & {
+  type: ColumnConfigType.BookingQuestionResponse;
+  booking_question_id: string | number;
 };
 
 function isColumnConfigType(type: string): type is ColumnConfigType {
@@ -38,15 +39,18 @@ function isColumnConfigType(type: string): type is ColumnConfigType {
 }
 
 function toJson(columnsConfigs: ColumnConfig[]): string {
-  return JSON.stringify(columnsConfigs)
+  return JSON.stringify(columnsConfigs);
 }
 
-type ColumnConfig = DefaultColumnConfig | UsageColumnConfig | CostColumnConfig;
+type ColumnConfig = BaseColumnConfig | UsageColumnConfig | BookingQuestionResponseColumnConfig;
 
 export default function ColumnsConfigFrom({ json, name }: ColumnsConfigFormProps) {
   const { t } = useTranslation();
   const [columnsConfig, setColumnsConfig] = useState<ColumnConfig[]>(() =>
-    (JSON.parse(json) as unknown as ColumnConfig[]).map((data, index) => ({ ...data, index, id: crypto.randomUUID() })),
+    (JSON.parse(json) as unknown as ColumnConfig[]).map((data) => ({
+      ...data,
+      id: data.id ? data.id : crypto.randomUUID(),
+    })),
   );
 
   const handleUpdate = (updatedConfig: ColumnConfig) =>
@@ -62,7 +66,13 @@ export default function ColumnsConfigFrom({ json, name }: ColumnsConfigFormProps
   return (
     <Form.Group className="mb-3">
       <Form.Label>{t("activerecord.attributes.data_digest_template.columns_config")}</Form.Label>
-      <ReactSortable tag="ol" className="list-group mb-3" list={columnsConfig} setList={setColumnsConfig} handle=".sortable-handle">
+      <ReactSortable
+        tag="ol"
+        className="list-group mb-3"
+        list={columnsConfig}
+        setList={setColumnsConfig}
+        handle=".sortable-handle"
+      >
         {columnsConfig.map((config) => (
           <li key={config.id} className="list-group-item">
             <ColumnConfigForm config={config} onRemove={handleRemove} onUpdate={handleUpdate}></ColumnConfigForm>
@@ -94,64 +104,66 @@ type ColumnConfigFormProps = {
 };
 
 function ColumnConfigForm({ config, onUpdate, onRemove }: ColumnConfigFormProps) {
-  const typeSpecificComponents = {
-    [ColumnConfigType.Default]: () => <></>,
-    [ColumnConfigType.Costs]: () => <></>,
-    [ColumnConfigType.Usage]: () => (
+  const typeSpecificComponents: Record<ColumnConfigType, (config: ColumnConfig) => React.ReactElement> = {
+    [ColumnConfigType.Default]: (_config) => <></>,
+    [ColumnConfigType.Costs]: (_config) => <></>,
+    [ColumnConfigType.Usage]: (config) => (
       <FloatingLabel label="Tarif ID">
         <Form.Control
           type="text"
-          defaultValue={config.tarif_id}
-          onChange={(event) => onUpdate({ ...config, tarif_id: event.target.value })}
+          defaultValue={(config as UsageColumnConfig).tarif_id}
+          onChange={(event) => onUpdate({ ...(config as UsageColumnConfig), tarif_id: event.target.value })}
         ></Form.Control>
       </FloatingLabel>
     ),
-    [ColumnConfigType.BookingQuestionResponse]: () => (
+    [ColumnConfigType.BookingQuestionResponse]: (config) => (
       <FloatingLabel label="Booking Question ID">
         <Form.Control
           type="text"
-          defaultValue={config.id}
-          onChange={(event) => onUpdate({ ...config, id: event.target.value })}
+          defaultValue={(config as BookingQuestionResponseColumnConfig).booking_question_id}
+          onChange={(event) =>
+            onUpdate({ ...(config as BookingQuestionResponseColumnConfig), booking_question_id: event.target.value })
+          }
         ></Form.Control>
       </FloatingLabel>
     ),
   };
 
   return (
-      <Row className="row-gap-2">
-        <Col md={1} className="align-self-center">
-          <span className="sortable-handle fa fa-bars"></span>
-        </Col>
-        <Col>
-          <Row className="row-gap-2">
-            <Col md={6}>
-              <FloatingLabel label="Header">
-                <Form.Control
-                  type="text"
-                  defaultValue={config.header}
-                  onChange={(event) => onUpdate({ ...config, header: event.target.value })}
-                ></Form.Control>
-              </FloatingLabel>
-            </Col>
-            <Col md={6}>
-              <FloatingLabel label="Body">
-                <Form.Control
-                  type="text"
-                  defaultValue={config.body}
-                  onChange={(event) => onUpdate({ ...config, body: event.target.value })}
-                ></Form.Control>
-              </FloatingLabel>
-            </Col>
-            <Col md={12}>{typeSpecificComponents[config.type]?.()}</Col>
-          </Row>
-        </Col>
-        <Col md={1} className="align-self-center">
-          <div className="btn-group">
-            <button type="button" className="btn btn-default" onClick={() => onRemove(config)}>
-              <span className="fa fa-trash"></span>
-            </button>
-          </div>
-        </Col>
-      </Row>
+    <Row className="row-gap-2">
+      <Col md={1} className="sortable-handle align-self-center">
+        <span className="fa fa-bars"></span>
+      </Col>
+      <Col>
+        <Row className="row-gap-2">
+          <Col md={6}>
+            <FloatingLabel label="Header">
+              <Form.Control
+                type="text"
+                defaultValue={config.header}
+                onChange={(event) => onUpdate({ ...config, header: event.target.value })}
+              ></Form.Control>
+            </FloatingLabel>
+          </Col>
+          <Col md={6}>
+            <FloatingLabel label="Body">
+              <Form.Control
+                type="text"
+                defaultValue={config.body}
+                onChange={(event) => onUpdate({ ...config, body: event.target.value })}
+              ></Form.Control>
+            </FloatingLabel>
+          </Col>
+          <Col md={12}>{typeSpecificComponents[config.type]?.(config)}</Col>
+        </Row>
+      </Col>
+      <Col md={1} className="align-self-center">
+        <div className="btn-group">
+          <button type="button" className="btn btn-default" onClick={() => onRemove(config)}>
+            <span className="fa fa-trash"></span>
+          </button>
+        </div>
+      </Col>
+    </Row>
   );
 }
