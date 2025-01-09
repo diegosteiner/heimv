1e6e66e716355aff19efb88c4ab15364b0f60033
Diego Steiner <diego.steiner@u041.ch>
2025-01-09 19:45:22 +0000

    fix: editable states default

--- app/models/booking_state_settings.rb
+++ app/models/booking_state_settings.rb
@@ -7,10 +7,10 @@ class BookingStateSettings
 
   attribute :default_manage_transition_to_state, :string, default: -> { BookingStates::ProvisionalRequest.to_s }
   attribute :occupied_occupancy_states, array: true, default: lambda {
-    BookingFlows::Default.occupied_by_default.map(&:to_sym)
+    BookingFlows::Default.occupied_by_default.map(&:to_s)
   }
   attribute :editable_occupancy_states, array: true, default: lambda {
-    BookingFlows::Default.editable_by_default.map(&:to_sym)
+    BookingFlows::Default.editable_by_default.map(&:to_s)
   }
 
   def self.manage_transition_to_states(organisation)
