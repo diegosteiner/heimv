e6c9d44e64e9208da6c34bf23cbb9e95c9de2623
Diego Steiner <diego.steiner@u041.ch>
2025-02-27 09:06:28 +0000

    feat: add conditions component

---
+++ app/javascript/components/booking_conditions/BookingCondition.tsx
@@ -0,0 +1,123 @@
+import { useTranslation } from "react-i18next";
+import BookingConditionCollection from "./BookingConditionCollection";
+import BookingConditionComparable from "./BookingConditionComparable";
+import { Dropdown } from "react-bootstrap";
+
+export enum BookingConditionType {
+  AlwaysApply = "BookingConditions::AlwaysApply",
+  BookingAttribute = "BookingConditions::BookingAttribute",
+  BookingCategory = "BookingConditions::BookingCategory",
+  BookingDateTime = "BookingConditions::BookingDateTime",
+  BookingQuestion = "BookingConditions::BookingQuestion",
+  BookingState = "BookingConditions::BookingState",
+  OccupancyDuration = "BookingConditions::OccupancyDuration",
+  Occupiable = "BookingConditions::Occupiable",
+  Tarif = "BookingConditions::Tarif",
+  TenantAttribute = "BookingConditions::TenantAttribute",
+  AndGroup = "BookingConditions::AndGroup",
+}
+
+export type BookingConditionComparableType =
+  | BookingConditionType.AlwaysApply
+  | BookingConditionType.BookingAttribute
+  | BookingConditionType.BookingCategory
+  | BookingConditionType.BookingDateTime
+  | BookingConditionType.BookingQuestion
+  | BookingConditionType.BookingState
+  | BookingConditionType.OccupancyDuration
+  | BookingConditionType.Occupiable
+  | BookingConditionType.Tarif
+  | BookingConditionType.TenantAttribute;
+
+export type BookingConditionCollectionType = BookingConditionType.AndGroup;
+
+export type BookingCondition =
+  | {
+      type: BookingConditionComparableType;
+      id: string;
+      compare_attribute?: string;
+      compare_operator?: string;
+      compare_value?: string;
+    }
+  | {
+      type: BookingConditionCollectionType;
+      id: string;
+      conditions?: BookingCondition[];
+    };
+
+export type BookingConditionOptionsForSelect = {
+  [T in BookingConditionType]: {
+    type: T;
+    name: string;
+    compare_attributes?: [string, string][] | null;
+    compare_operators?: [string, string][] | null;
+    compare_values?: [string, string][] | null;
+  };
+};
+
+export function initializeBookingCondition(
+  condition: Partial<BookingCondition> & Pick<BookingCondition, "type">,
+): BookingCondition {
+  return { id: crypto.randomUUID(), ...condition };
+}
+
+type BookingConditionElementProps = {
+  condition: BookingCondition;
+  optionsForSelect: BookingConditionOptionsForSelect;
+  onRemove?: (condition: BookingCondition) => void;
+  onChange?: (condition: BookingCondition) => void;
+};
+
+export function BookingConditionElement({
+  condition,
+  optionsForSelect,
+  onRemove,
+  onChange,
+}: BookingConditionElementProps) {
+  if (condition.type === BookingConditionType.AndGroup) {
+    return (
+      <BookingConditionCollection
+        condition={condition}
+        optionsForSelect={optionsForSelect}
+        onRemove={onRemove}
+        onChange={onChange}
+      />
+    );
+  }
+
+  return (
+    <BookingConditionComparable
+      condition={condition}
+      optionsForSelect={optionsForSelect}
+      onRemove={onRemove}
+      onChange={onChange}
+    />
+  );
+}
+
+export function AddBookingConditionDropdown({
+  onAction,
+  disabled,
+  optionsForSelect,
+}: {
+  onAction: (type: BookingConditionType) => void;
+  optionsForSelect: BookingConditionOptionsForSelect;
+  disabled?: boolean;
+}) {
+  const { t } = useTranslation();
+
+  return (
+    <Dropdown onSelect={(eventKey) => !disabled && onAction(eventKey as BookingConditionType)}>
+      <Dropdown.Toggle variant="primary">
+        {t("add_record", { model_name: t("activemodel.models.booking_condition.one") })}
+      </Dropdown.Toggle>
+      <Dropdown.Menu>
+        {Object.values(BookingConditionType).map((type) => (
+          <Dropdown.Item disabled={disabled} eventKey={type} key={type}>
+            {optionsForSelect[type].name}
+          </Dropdown.Item>
+        ))}
+      </Dropdown.Menu>
+    </Dropdown>
+  );
+}
---
+++ app/javascript/components/booking_conditions/BookingConditionForm.tsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import {
+  BookingConditionElement,
+  initializeBookingCondition,
+  type BookingConditionType,
+  type BookingCondition,
+  type BookingConditionOptionsForSelect,
+  AddBookingConditionDropdown,
+} from "./BookingCondition";
+
+type Props = {
+  value?: BookingCondition[];
+  name: string;
+  disabled: boolean;
+  optionsForSelect: BookingConditionOptionsForSelect;
+};
+
+export default function BookingConditionForm({ optionsForSelect, value, name, disabled }: Props) {
+  const [conditions, setConditions] = useState<BookingCondition[]>(value || []);
+
+  const handleAdd = (type: BookingConditionType) =>
+    !disabled &&
+    setConditions((prev) => {
+      return [...prev, initializeBookingCondition({ type })];
+    });
+  const handleRemove = (removedCondition: BookingCondition) =>
+    !disabled && setConditions((prev) => prev.filter((prevCondition) => prevCondition.id !== removedCondition.id));
+  const handleChange = (changedCondition: BookingCondition) =>
+    !disabled &&
+    setConditions((prev) =>
+      prev.map((prevCondition) => (prevCondition.id === changedCondition.id ? changedCondition : prevCondition)),
+    );
+
+  return (
+    <div className="mb-3">
+      <input type="hidden" name={name} disabled={disabled} value={JSON.stringify(conditions)} />
+      <ol className="list-group mb-3">
+        {Array.from(conditions).map((condition) => (
+          <li key={condition.id} className="list-group-item">
+            <BookingConditionElement
+              condition={condition}
+              optionsForSelect={optionsForSelect}
+              onChange={handleChange}
+              onRemove={handleRemove}
+            />
+          </li>
+        ))}
+      </ol>
+      <AddBookingConditionDropdown disabled={disabled} onAction={handleAdd} optionsForSelect={optionsForSelect} />
+    </div>
+  );
+}
