1ecc28f162da5eff1b4905b19323596bc3d80cd0
Diego Steiner <diego.steiner@u041.ch>
2025-04-30 15:26:22 +0000

    fix: email_invoice template context

--- app/jobs/mail_delivery_job.rb
+++ app/jobs/mail_delivery_job.rb
@@ -3,4 +3,10 @@
 class MailDeliveryJob < ActionMailer::MailDeliveryJob
   retry_on Net::SMTPFatalError, Net::SMTPAuthenticationError, Net::ReadTimeout, ActiveJob::DeserializationError,
            wait: 30.seconds, attempts: 5
+
+  rescue_from(Exception) do |exception|
+    ExceptionNotifier.notify_exception(exception) if defined?(ExceptionNotifier)
+    Rails.error.report(exception)
+    raise exception
+  end
 end
