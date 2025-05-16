d53f77bb7618352b0b21bf666f47fdae6f7436e2
Diego Steiner <diego.steiner@u041.ch>
2025-05-16 08:02:19 +0000

    fix: retry jobs and mails

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 class ApplicationJob < ActiveJob::Base
-  rescue_from(Exception) do |exception|
+  after_discard do |job, exception|
     Rails.error.report(exception)
-    ExceptionNotifier.notify_exception(exception, data: serialize) if defined?(ExceptionNotifier)
+    ExceptionNotifier.notify_exception(exception, data: job.serialize) if defined?(ExceptionNotifier)
     raise exception
   end
 end
