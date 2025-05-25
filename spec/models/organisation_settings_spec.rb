bae3f534b71008dfe8f3f415d23728d4cb830435
Diego Steiner <diego.steiner@u041.ch>
2025-05-25 10:01:57 +0000

    feat: add basic implementation for the xoauth2 protocol for smtp

--- spec/models/organisation_settings_spec.rb
+++ spec/models/organisation_settings_spec.rb
@@ -1,3 +1,15 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
+
+RSpec.describe OrganisationSettings do
+  subject(:settings) { organisation.settings }
+
+  let(:organisation) { create(:organisation) }
+
+  before { organisation.settings = {} }
+
+  describe 'valid?' do
+    it { is_expected.to be_valid }
+  end
+end
