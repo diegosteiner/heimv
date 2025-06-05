9d9db093823332f4954d2027c7b4f149573a3fbc
Diego Steiner <diego.steiner@u041.ch>
2025-06-05 19:25:22 +0000

    chore: update cve fix

--- app/models/payment_infos/qr_bill.rb
+++ app/models/payment_infos/qr_bill.rb
@@ -91,7 +91,7 @@ module PaymentInfos
 
     def checksum(ref)
       base97_digits = ref.upcase.chars.map { CHAR_TABLE.index(it) } + RF00
-      98 - (base97_digits.flatten.join.to_i % 97)
+      98 - (base97_digits.join.to_i % 97)
       # 98 - base97_digits.in_groups_of(7).reduce(base97_digits.shift(2)) do |check, digits|
       # ((check + digits).flatten.join.to_i % 97).to_s.chars
       # end.join.to_i
