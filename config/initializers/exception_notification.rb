4df660a6c2311c94facce35eae8228a0f0731ee7
Diego Steiner <diego.steiner@u041.ch>
2025-06-09 09:49:02 +0000

    fix: error regression

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -2,7 +2,7 @@
 
 if defined?(ExceptionNotification)
   Rails.application.config.middleware.use(
-    ExceptionNotification::Rack, email: {
+    ExceptionNotification::Rails, email: {
       sender_address: ENV.fetch('MAIL_FROM', nil),
       email_prefix: "[#{ENV.fetch('APP_HOST', nil)}] ",
       exception_recipients: ENV.fetch('EXCEPTION_RECIPIENTS', 'info@heimv.ch').split(',')
