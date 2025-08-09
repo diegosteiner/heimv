100622ea4f90756d3955c22b16f3f1de782c64a3
Diego Steiner <diego.steiner@u041.ch>
2025-08-09 15:49:43 +0000

    fix: transition job error resilience

--- app/jobs/transition_booking_states_job.rb
+++ app/jobs/transition_booking_states_job.rb
@@ -3,7 +3,7 @@
 class TransitionBookingStatesJob < ApplicationJob
   queue_as :default
 
-  def perform(bookings = Booking.where(concluded: false))
+  def perform(bookings = Booking.where(concluded: false)) # rubocop:disable Metrics/MethodLength
     job_transitions = {}
     bookings.with_default_includes.find_each do |booking|
       transitions = booking.apply_transitions&.compact_blank
@@ -11,6 +11,9 @@ class TransitionBookingStatesJob < ApplicationJob
 
       Booking::Log.log(booking, trigger: :auto, data: { transitions: })
       job_transitions[booking.id] = transitions
+    rescue ActiveRecord::RecordInvalid => e
+      Rails.error.report(exception)
+      ExceptionNotifier.notify_exception(e, data: booking.attributes) if defined?(ExceptionNotifier)
     end
 
     job_transitions.compact_blank
