343694ef049d8a6ac4eb58024d25b60bcf218ec0
Diego Steiner <diego.steiner@u041.ch>
2025-08-19 13:04:56 +0000

    fix: migrations

--- db/migrate/20250819103045_add_items_to_invoices.rb
+++ db/migrate/20250819103045_add_items_to_invoices.rb
@@ -23,18 +23,26 @@ class AddItemsToInvoices < ActiveRecord::Migration[8.0]
 
   def migrate_invoice_parts
     table = Arel::Table.new(:invoice_parts)
-    query = table.project(Arel.star).order(table[:ordinal])
-    ActiveRecord::Base.connection.exec_query(query.to_sql).to_a.each do |invoice_part|
-      invoice_part.symbolize_keys!
-      item_hash = invoice_part.slice(*%i[apply accounting_account_nr accounting_cost_center_nr id
-                                         amount breakdown label type usage_id vat_category_id ])
-      item_hash[:type] = MAP_TYPES[item_hash[:type]]
-      invoice = Invoice.find(invoice_part[:invoice_id])
-      invoice.items ||= []
-      invoice.items << Invoice::Item.one_of.to_type.cast_value(item_hash)
-      invoice.skip_generate_pdf = true
-      invoice.skip_journal_entry_batches = true
-      invoice.save!
+    batch_size = 100
+    offset = 0
+    loop do
+      batch_query = table.project(Arel.star).order(table[:invoice_id], table[:ordinal]).take(batch_size).skip(offset) # Arel adds LIMIT/OFFSET
+      rows = ActiveRecord::Base.connection.exec_query(batch_query.to_sql)
+      break if rows.empty?
+      offset += batch_size
+
+      rows.each do |invoice_part|
+        invoice_part.symbolize_keys!
+        item_hash = invoice_part.slice(*%i[apply accounting_account_nr accounting_cost_center_nr id
+                                          amount breakdown label type usage_id vat_category_id ])
+        item_hash[:type] = MAP_TYPES[item_hash[:type]]
+        invoice = Invoice.find(invoice_part[:invoice_id])
+        invoice.items ||= []
+        invoice.items << Invoice::Item.one_of.to_type.cast_value(item_hash)
+        invoice.skip_generate_pdf = true
+        invoice.skip_journal_entry_batches = true
+        invoice.save!
+      end
     end
   end
 
