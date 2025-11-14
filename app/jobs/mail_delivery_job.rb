18e41ebaad36119418bb8f06721ed140a8aa41eb
Diego Steiner <diego.steiner@u041.ch>
2025-11-14 12:06:35 +0000

    fix: email validation

--- app/jobs/mail_delivery_job.rb
+++ app/jobs/mail_delivery_job.rb
@@ -5,7 +5,7 @@ class MailDeliveryJob < ActionMailer::MailDeliveryJob
            wait: 30.seconds, attempts: 5
 
   after_discard do |job, exception|
-    Rails.error.report(exception)
+    Rails.logger.error(exception.message)
     ExceptionNotifier.notify_exception(exception, data: job.serialize) if defined?(ExceptionNotifier)
     raise exception
   end
