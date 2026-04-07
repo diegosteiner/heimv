4d8fb4978c3b2b4dbeceb98fad1d59743173a7a5
Diego Steiner <diego.steiner@u041.ch>
2026-04-07 15:11:35 +0200

    fix: mark_contract_signed

--- db/migrate/20260330164646_fix_booking_occupancy_states.rb
+++ db/migrate/20260330164646_fix_booking_occupancy_states.rb
@@ -5,7 +5,6 @@ class FixBookingOccupancyStates < ActiveRecord::Migration[8.1]
     # free_states = %i[declined_request cancelled_request cancelled]
     # bookings = Booking.joins(:state_transitions)
     #           .where(state_transitions: { most_recent: true, to_state: free_states }, concluded: true)
-
     # Occupancy.where(occupancy_type: :free, booking_id: nil)
     #          .update_all(occupancy_type: :reserved, ignore_conflicting: true)
   end
