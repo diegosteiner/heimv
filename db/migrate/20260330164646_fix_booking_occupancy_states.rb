efda830fc4ceb3982655b16b44722e8ace7f5aff
Diego Steiner <diego.steiner@u041.ch>
2026-04-07 15:11:35 +0200

    fix: mark_contract_signed

--- db/migrate/20260330164646_fix_booking_occupancy_states.rb
+++ db/migrate/20260330164646_fix_booking_occupancy_states.rb
@@ -2,12 +2,12 @@
 
 class FixBookingOccupancyStates < ActiveRecord::Migration[8.1]
   def up
-    free_states = %i[declined_request cancelled_request cancelled]
-    Booking.joins(:state_transitions)
-           .where(state_transitions: { most_recent: true, to_state: free_states }, concluded: true)
-           .find_each { it.update(occupancy_type: :free) }
+    # free_states = %i[declined_request cancelled_request cancelled]
+    # bookings = Booking.joins(:state_transitions)
+    #           .where(state_transitions: { most_recent: true, to_state: free_states }, concluded: true)
 
-    Occupancy.where(occupancy_type: :free, booking_id: nil)
-             .update_all(occupancy_type: :reserved, ignore_conflicting: true) # rubocop:disable Rails/SkipsModelValidations
+
+    # Occupancy.where(occupancy_type: :free, booking_id: nil)
+    #          .update_all(occupancy_type: :reserved, ignore_conflicting: true) # rubocop:disable Rails/SkipsModelValidations
   end
 end
