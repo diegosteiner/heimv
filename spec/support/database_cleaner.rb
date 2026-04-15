8db503f917c60478477312120aa174dff4cdcc8b
Diego Steiner <diego.steiner@u041.ch>
2026-04-15 21:29:13 +0200

    fix: ci

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -7,8 +7,8 @@ RSpec.configure do |config|
   end
 
   config.before(:each, type: :feature) do
-    DatabaseCleaner.strategy = :truncation
-    DatabaseCleaner.clean_with(:truncation)
+    DatabaseCleaner.strategy = :deletion
+    DatabaseCleaner.clean_with(:deletion)
   end
 
   config.before do
